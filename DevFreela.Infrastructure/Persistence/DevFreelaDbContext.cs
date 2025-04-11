9b03bd91c1f23ccb30e17b0abcb74f8bab628d6e
Gustavo <renegustavo42@gmail.com>
2025-04-11 08:19:27 -0300

    feat: create layer infrastructure

---
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace DevFreela.Infrastructure.Persistence;
+
+public class DevFreelaDbContext : DbContext
+{
+    public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
+    {
+        
+    }
+
+    public List<Project> Projects { get; set; }
+    public List<User> Users { get; set; }
+    public List<Skill> Skills { get; set; }
+    public List<UserSkill> UserSkills { get; set; }
+    public List<ProjectComment> ProjectComments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());  
+    }
+}
